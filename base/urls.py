18dc5eb13735fc85e31558722c2012faa3527d24
VolodumurLesyuk <5647382910z8@gmail.com>
2023-02-20 19:32:41 +0200

    Final version of program

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 
-from .views import room, lobby
+from .views import (
+    room,
+    lobby,
+    getToken,
+    createMember,
+    deleteMember,
+    getMember,
+)
 
 
 urlpatterns = [
     path('', lobby),
     path('room/', room),
+    path('get_token/', getToken),
+    path('create_member/', createMember),
+    path('get_member/', getMember),
+    path('delete_member/', deleteMember),
 ]
--- static/js/config.js
+++
@@ -1,2 +0,0 @@
-sessionStorage.setItem('APP_ID', '6d928d60189c4cb19dc28cae844bf1af');
-sessionStorage.setItem('TOKEN', '007eJxTYNi2ObJ2/8THvK/vBM5+v/fN5qB0c9vJbcfdV+43ZA34PkdcgcEsxdLIIsXMwNDCMtkkOcnQMiXZyCI5MdXCxCQpzTAx7cCi18kNgYwM1a8TWBkZIBDE52TITczMK8tMSc1nYAAA2FMkuw==');
